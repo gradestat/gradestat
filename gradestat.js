e34c7319b9384b1e2e1d05fd15aa552414926ff7
alecguertin <alecguertin@berkeley.edu>
2015-03-15 01:52:10 -0700

    layout and not found working with panda img

--- gradestat.js
+++ gradestat.js
@@ -1,3 +1,8 @@
+Router.configure({
+    layoutTemplate: "layout",
+    notFoundTemplate: "not-found",
+});
+
 Router.map(function() {
     this.route('splash', {path: '/'});
     this.route('about');
