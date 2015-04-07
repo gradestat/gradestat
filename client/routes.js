ebc5003b7772b7d0bdf9fcaaac8523c2864146f5
Michael Ball <cycomachead@gmail.com>
2015-04-07 00:59:57 -0700

    Refactoring and login views work!!!

--- client/routes.js
+++ client/routes.js
@@ -1,13 +1,38 @@
 Router.configure({
-  layoutTemplate: 'layout',
+  layoutTemplate: 'main',
   notFoundTemplate: "not-found"
 });
 
+
+// Simple Routes where template name == route Name
 Router.map(function() {
-  this.route('splash', {
-      path: '/'
-  });
-  this.route('about');
-  this.route('dashboard');
+    this.route('splash', {
+        path: '/'
+    })
+    this.route('about');
+    this.route('dashboard');
 });
 
+// Helper Functions
+var enforceLogin = function() {
+    if (!Meteor.user()) {
+        if (Meteor.loggingIn()) {
+
+        } else {
+            this.render('splash');
+        }
+    } else {
+        this.next();
+    }
+};
+
+var showDashboard = function() {
+    if (Meteor.user()) {
+        this.render('dashboard');
+    } else {
+        this.next();
+    }
+}
+
+Router.onBeforeAction(showDashboard, {only: 'splash'})
+// Router.onBeforeAction(enforceLogin)
