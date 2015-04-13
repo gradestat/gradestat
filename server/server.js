893adfb43b261aa132a61e4cc22be0335de84ef4
alecguertin <alecguertin@berkeley.edu>
2015-04-13 15:06:02 -0700

    partially done canvas token input

--- server/server.js
+++ server/server.js
@@ -1,3 +1,15 @@
+// Serverside routes
+Router.map(function() {
+    this.route('update_token', {
+	where: 'server',
+	action: function () {
+	    var user = Meteor.user();
+	    user.canvas_token = this.request.query.access_token;
+	},
+    });
+});
+
+
 // DB Setup
 // Users "collection" is created for us
 
