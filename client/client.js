8d124f037eb8dbaa6c6d2b88dd57f7d77697356c
alecguertin <alecguertin@berkeley.edu>
2015-04-14 15:09:38 -0700

    made dashboard reactive-ish, renders different templates based on which sidebar button you click

--- client/client.js
+++ client/client.js
@@ -1,8 +1,26 @@
+Session.setDefault("dashView", 'home');
+
+Template.dashboard.events({
+    'click #home': function(e) {
+	Session.set('dashView', 'home');
+    },
+    'click #settings': function(e) {
+	Session.set('dashView', 'settings');
+    },
+    'click #courses': function(e) {
+	Session.set('dashView', 'courses');
+    }
+});
+
+
 Tracker.autorun(function() {
     Meteor.subscribe("userData");
 });
 
 Template.dashboard.helpers({
+    dashView: function() {
+	return Session.get("dashView");
+    },
     canvasToken: function() {
 	var user = Meteor.user();
 	return user.canvasToken;
