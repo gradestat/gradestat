4fe90b64409ead27ae2635cca887b414ac30c0d6
alecguertin <alecguertin@berkeley.edu>
2015-04-27 00:59:03 -0700

    sigmas working

--- client/javascript/assignment.js
+++ client/javascript/assignment.js
@@ -57,14 +57,15 @@ setHistoData = function(ret) {
     }];
     var sigmas = [-3,-2,-1,1,2,3];
     for (s in sigmas) {
+	console.log(s);
 	plotLines.push({
-	    "value": mean + sd*s,
+	    "value": mean + sd*sigmas[s],
             "width": 1,
             "color": "#999",
             "dashStyle": "Dash",
             "zIndex": 10,
             "label": {
-		"text": s.toString() + "s",
+		"text": sigmas[s].toString() + "s",
 		"rotation": 0,
 		"align": "center",
 		"x": 0,
@@ -75,19 +76,21 @@ setHistoData = function(ret) {
 	    }
 	});
     }
+    console.log(mean);
+    console.log(plotLines);
     var plotBands = [{
-	"from": -1*sd,
-        "to": 1*sd,
+	"from": -1*sd + mean,
+        "to": 1*sd + mean,
         "color": "rgba(184,210,236,.1)",
         "zIndex": 0
     }, {
-	"from": -2*sd,
-        "to": 2*sd,
+	"from": -2*sd + mean,
+        "to": 2*sd + mean,
         "color": "rgba(184,210,236,.1)",
         "zIndex": 0
     }, {
-	"from": -3*sd,
-        "to": 3*sd,
+	"from": -3*sd + mean,
+        "to": 3*sd + mean,
         "color": "rgba(184,210,236,.1)",
         "zIndex": 0
     }];
@@ -101,13 +104,21 @@ setHistoData = function(ret) {
     while (curBucket < maxPoints) {
 	count = 0;
 	for (var i = 0; i < scores.length; i += 1) {
-	    if (scores[i] > curBucket - bucketMargin && scores[i] < curBucket + bucketMargin) {
+	    if (scores[i] >= curBucket - bucketMargin && scores[i] < curBucket + bucketMargin) {
 		count += 1;
 	    }
 	}
 	buckets.push([curBucket, count]);
-	curBucket += bucketMargin;
+	curBucket += bucketSize;
     }
+    count = 0;
+    for (var j = 0; j < scores.length; j += 1) {
+	if (scores[j] == maxPoints) {
+	    count += 1;
+	}
+    }
+    buckets[buckets.length-1][1] += count;
+    console.log(buckets);
     Session.set("histoData", {
 	chart: {
             type: 'column',
@@ -165,73 +176,13 @@ setHistoData = function(ret) {
 	},
 	series: [{
             name: 'Sample Distribution',
-            data: buckets;
-	    // [
-	    // 	[-2.3138013781265, 1],
-	    // 	[-2.0943590644815, 4],
-	    // 	[-1.8749167508365, 11],
-	    // 	[-1.6554744371915, 12],
-	    // 	[-1.4360321235466, 18],
-	    // 	[-1.2165898099016, 18],
-	    // 	[-0.99714749625658, 24],
-	    // 	[-0.77770518261159, 21],
-	    // 	[-0.55826286896661, 36],
-	    // 	[-0.33882055532162, 40],
-	    // 	[-0.11937824167663, 51],
-	    // 	[0.10006407196835, 40],
-	    // 	[0.31950638561334, 42],
-	    // 	[0.53894869925832, 36],
-	    // 	[0.75839101290331, 40],
-	    // 	[0.9778333265483, 36],
-	    // 	[1.1972756401933, 23],
-	    // 	[1.4167179538383, 18],
-	    // 	[1.6361602674833, 9],
-	    // 	[1.8556025811282, 12],
-	    // 	[2.0750448947732, 3],
-	    // 	[2.2944872084182, 4]
-            // ],
-            pointRange: 0.21944231364499,
+            data: buckets,
+            pointRange: bucketSize,
             borderWidth: .5,
             borderColor: '#666',
-            pointPadding: .015,
+            pointPadding: 0,
             groupPadding: 0,
             color: '#e3e3e3'
-	}, {
-            type: 'spline',
-            lineWidth: 1,
-            name: 'Normal Distribution',
-            color: 'rgba(90,155,212,.75)',
-            fillColor: 'rgba(90,155,212,.15)',
-            data: [
-		[-3.2807020192309, 0.10168053006185],
-		[-3.0425988742109, 0.23641431548771],
-		[-2.8044957291909, 0.51637633957668],
-		[-2.5663925841709, 1.0595354537927],
-		[-2.3282894391509, 2.0423080409267],
-		[-2.0901862941309, 3.6981421093266],
-		[-1.8520831491109, 6.2907516383431],
-		[-1.6139800040909, 10.052592494842],
-		[-1.3758768590709, 15.090728685704],
-		[-1.1377737140509, 21.28133847858],
-		[-0.89967056903087, 28.193192861774],
-		[-0.66156742401087, 35.086995418605],
-		[-0.42346427899086, 41.020853564556],
-		[-0.18536113397085, 45.052593912695],
-		[0.052742011049155, 46.482716760157],
-		[0.29084515606916, 45.052593912695],
-		[0.52894830108917, 41.020853564556],
-		[0.76705144610918, 35.086995418605],
-		[1.0051545911292, 28.193192861773],
-		[1.2432577361492, 21.28133847858],
-		[1.4813608811692, 15.090728685704],
-		[1.7194640261892, 10.052592494842],
-		[1.9575671712092, 6.2907516383431],
-		[2.1956703162292, 3.6981421093266],
-		[2.4337734612492, 2.0423080409267],
-		[2.6718766062692, 1.0595354537927],
-		[2.9099797512892, 0.51637633957668],
-		[3.1480828963092, 0.23641431548771]
-            ]
 	}, {
             type: 'area',
             name: 'Sigma Bands',
